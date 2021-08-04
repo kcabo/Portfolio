d9890f21297ce702ec91a592f4e9d78e414f3f99
kcabo <37576764+kcabo@users.noreply.github.com>
2021-08-04 23:47:03 +0900

    💄 Add Header

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header className='py-8'>
+      <div className='container mx-auto'>
+        <div className='w-10 mx-auto mb-3'>
+          <img src='logo.svg' alt='ロゴ' />
+        </div>
+        <nav className='flex justify-center gap-8 mx-auto'>
+          <a className='p-4 text-lg font-bold text-gray-600'>Work</a>
+          <a className='p-4 text-lg font-bold text-gray-600'>About</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
