0aa77f5b338250c8a96beb15f8b68e8651c9950c
kcabo <k7cabo@gmail.com>
2021-08-05 10:03:36 +0900

    💄 add footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,3 @@
-import { GetStaticProps } from 'next';
-import { client } from '../libs/client';
-import styles from '../styles/Home.module.css';
-
-type BlogResponse = {
-  id: string;
-  createdAt: string;
-  updatedAt: string;
-  publishedAt: string;
-  revisedAt: string;
-  title: string;
-  article: string;
-};
-
-export default function Home(blog: BlogResponse) {
-  return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: `${blog.article}`,
-      }}
-      className={styles.article}
-    />
-  );
+export default function Home() {
+  return <div className='text-4xl'>こんにちは</div>;
 }
-
-export const getStaticProps: GetStaticProps = async () => {
-  const data = await client.get<BlogResponse>({
-    endpoint: 'works',
-    contentId: '22u8obhjn_p',
-  });
-
-  return {
-    props: data,
-  };
-};
