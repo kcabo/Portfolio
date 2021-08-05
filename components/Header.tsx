b0cfeaaa3d454395e065902904ae85e2069d4f81
kcabo <k7cabo@gmail.com>
2021-08-05 10:24:57 +0900

    ✨ Add link to nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,23 @@
+import Link from 'next/link';
+
 export default function Header() {
   return (
     <header className='py-10'>
       <div className='container mx-auto'>
         <div className='w-10 mx-auto mb-2'>
-          <img src='logo.svg' alt='ロゴ' />
+          <Link href='/'>
+            <a>
+              <img src='logo.svg' alt='ロゴ' />
+            </a>
+          </Link>
         </div>
         <nav className='flex justify-center gap-8 mx-auto'>
-          <a className='p-4 text-lg font-bold text-gray-600'>Work</a>
-          <a className='p-4 text-lg font-bold text-gray-600'>About</a>
+          <Link href='/works'>
+            <a className='p-4 text-lg font-bold text-gray-600'>Work</a>
+          </Link>
+          <Link href='/about'>
+            <a className='p-4 text-lg font-bold text-gray-600'>About</a>
+          </Link>
         </nav>
       </div>
     </header>
