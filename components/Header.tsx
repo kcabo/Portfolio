2ce8f05b9570753a06aeb5c09a0e8a25fe2b52ea
kcabo <37576764+kcabo@users.noreply.github.com>
2021-08-05 18:48:29 +0900

    ♻️ replace main logo with next/image

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,21 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header() {
   return (
     <header className='pt-12 pb-6 md:pb-10'>
       <div className='container mx-auto'>
-        <div className='w-10 mx-auto mb-2'>
-          <Link href='/'>
-            <a>
-              <img src='logo.svg' alt='ロゴ' />
-            </a>
-          </Link>
-        </div>
+        <Link href='/'>
+          <a className='block w-10 mx-auto mb-2'>
+            <Image src='/logo.svg' alt='kcabo logo' height={40} width={40} />
+          </a>
+        </Link>
         <nav className='flex justify-center gap-8 mx-auto'>
           <Link href='/works'>
-            <a className='p-4 text-lg font-bold text-gray-600'>Work</a>
+            <a className='p-3 text-lg font-bold text-gray-600'>Work</a>
           </Link>
           <Link href='/about'>
-            <a className='p-4 text-lg font-bold text-gray-600'>About</a>
+            <a className='p-3 text-lg font-bold text-gray-600'>About</a>
           </Link>
         </nav>
       </div>
