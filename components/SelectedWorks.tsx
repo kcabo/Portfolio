57797c6b39976655303ec6215ac886ab4abeb471
kcabo <k7cabo@gmail.com>
2021-08-05 17:09:18 +0900

    ✨ add selected works UI

---
+++ components/SelectedWorks.tsx
@@ -0,0 +1,16 @@
+import HomeCaption from './HomeCaption';
+import WorkCard from './WorkCard';
+
+export default function SelectedWorks() {
+  return (
+    <div className=''>
+      <HomeCaption caption='Selected Works' allLink='/works' />
+      {/* トラックの高さ一杯にカードを広げる。見出しの高さを抜く */}
+      <div className='grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3'>
+        <WorkCard />
+        <WorkCard />
+        <WorkCard />
+      </div>
+    </div>
+  );
+}
