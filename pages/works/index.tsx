1d5df8a0431ac5fd792734d895e7a3f5ba1ffd81
kcabo <37576764+kcabo@users.noreply.github.com>
2021-08-07 00:31:09 +0900

    ✨ set up works list page

--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -1,12 +1,23 @@
-import Link from 'next/link';
+import WorkCard from '../../components/WorkCard';
 
-export default function Home() {
+export default function Works() {
   return (
     <div className=''>
-      <div>あいうえお</div>
-      <Link href='./works/aaa'>
-        <a>ワークページ</a>
-      </Link>
+      <div className='mb-8'>
+        <h1 className='mb-2 text-2xl font-bold text-gray-600'>My Works</h1>
+        <p className='text-gray-400'>
+          これまでに開発したアプリ・ツールの一覧です。
+        </p>
+      </div>
+      <div className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3'>
+        <WorkCard />
+        <WorkCard />
+        <WorkCard />
+        <WorkCard />
+        <WorkCard />
+        <WorkCard />
+        <WorkCard />
+      </div>
     </div>
   );
 }
