d02b4a4cdb47c47c9c7e2f8d898528d0016e990f
kcabo <37576764+kcabo@users.noreply.github.com>
2021-08-06 23:13:51 +0900

    💄 UI improvements

--- components/Activities.tsx
+++ components/Activities.tsx
@@ -6,7 +6,7 @@ export default function Activities() {
     <div className=''>
       <HomeCaption caption='Recent Activities' allLink='/logs' />
       {/* トラックの高さ一杯にカードを広げる。見出しの高さを抜く */}
-      <div className='h-[184px] lg:h-[207.21px] p-5 bg-white rounded-lg shadow-lg overflow-y-auto'>
+      <div className='md:h-[184px] lg:h-[207.21px] p-5 bg-white rounded-lg shadow-lg overflow-y-auto'>
         <Log />
         <Log />
       </div>
