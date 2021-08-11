27921a7acaeb0590da2f918476fb3f46da1616e3
kcabo <k7cabo@gmail.com>
2021-08-11 13:16:50 +0900

    ✨ make work card clickable

--- components/WorkCard.tsx
+++ components/WorkCard.tsx
@@ -4,35 +4,39 @@ import { ExternalLinkIcon } from '@heroicons/react/solid';
 
 export default function WorkCard() {
   return (
-    <div className='overflow-hidden bg-white rounded-lg shadow-lg'>
-      <div className='relative w-full h-48 border-b border-gray-300'>
-        <Image
-          src='/tim.png'
-          alt='サムネイル'
-          layout='fill'
-          objectFit='cover'
-        />
-      </div>
-      <div className='px-5 py-6'>
-        <div className='flex mb-2 align-baseline'>
-          <h4 className='mr-2 text-xl font-medium text-gray-600'>TiM</h4>
-          <Link href='/'>
-            <a className='px-1 pt-1'>
-              <ExternalLinkIcon className='w-5 h-5 text-gray-400' />
-            </a>
-          </Link>
+    <Link href='/works/[id]' as='/works/abc'>
+      <a>
+        <div className='overflow-hidden bg-white rounded-lg shadow-lg'>
+          <div className='relative w-full h-48 border-b border-gray-300'>
+            <Image
+              src='/tim.png'
+              alt='サムネイル'
+              layout='fill'
+              objectFit='cover'
+            />
+          </div>
+          <div className='px-5 py-6'>
+            <div className='flex mb-2 align-baseline'>
+              <h4 className='mr-2 text-xl font-medium text-gray-600'>TiM</h4>
+              <Link href='/'>
+                <a className='px-1 pt-1'>
+                  <ExternalLinkIcon className='w-5 h-5 text-gray-400' />
+                </a>
+              </Link>
+            </div>
+            <div className='mb-6 text-gray-500'>
+              部活でタイムを管理するためのLINE
+              Bot。対話形式でタイムを入力し、CSVに出力。
+            </div>
+            <div className='flex flex-wrap gap-3'>
+              <Tag />
+              <Tag />
+              <Tag />
+            </div>
+          </div>
         </div>
-        <div className='mb-6 text-gray-500'>
-          部活でタイムを管理するためのLINE
-          Bot。対話形式でタイムを入力し、CSVに出力。
-        </div>
-        <div className='flex flex-wrap gap-3'>
-          <Tag />
-          <Tag />
-          <Tag />
-        </div>
-      </div>
-    </div>
+      </a>
+    </Link>
   );
 }
 
