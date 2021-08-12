09d1e743e70edcf6259f79e3741db249d193bd03
kcabo <37576764+kcabo@users.noreply.github.com>
2021-08-12 16:03:10 +0900

    ♻️ move padding attribution to separate pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import SelectedWorks from '../components/SelectedWorks';
 
 export default function Home() {
   return (
-    <div className='grid grid-cols-1 gap-10 md:grid-cols-[minmax(300px,400px)minmax(300px,1fr)]'>
+    <div className='grid px-8 grid-cols-1 gap-10 md:grid-cols-[minmax(300px,400px)minmax(300px,1fr)]'>
       <BusinessCard />
       <Activities />
       <div className='md:col-span-2'>
