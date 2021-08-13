4aa786e1a31d29a2de63bbb732148c4535891e26
kcabo <37576764+kcabo@users.noreply.github.com>
2021-08-13 20:07:15 +0900

    ✨ make activities data dynamic

--- components/Log.tsx
+++ components/Log.tsx
@@ -1,17 +1,39 @@
-import { Zenn } from '@icons-pack/react-simple-icons';
+import { Github, Zenn } from '@icons-pack/react-simple-icons';
+import Logo from '../public/logo.svg';
 
-export default function Log() {
+type TLogType = 'GitHub' | 'Portfolio' | 'Zenn';
+
+type Props = {
+  showOnTop: boolean;
+  logType: TLogType;
+  date: string;
+  body: string;
+};
+
+export default function Log({ showOnTop, logType, date, body }: Props) {
   return (
     <div className='py-2'>
       <div className='flex mb-1'>
-        <Zenn color='#3EA8FF' size={18} />
-        {/* <img src='/logo.svg' alt='' width={18} /> */}
-        <div className='mx-2 font-medium leading-5 text-gray-600'>Zenn</div>
-        <div className='text-sm text-gray-400'>2021-07-12</div>
+        <Icon logType={logType} />
+        <div className='mx-2 font-medium leading-5 text-gray-600'>
+          {logType}
+        </div>
+        <div className='text-sm text-gray-400'>{date}</div>
       </div>
       <h3 className='text-sm leading-relaxed text-gray-600 lg:text-base'>
-        「Github 周りのいい感じのバッジが欲しい」を投稿しました。
+        {body}
       </h3>
     </div>
   );
+}
+
+function Icon({ logType }: { logType: TLogType }) {
+  switch (logType) {
+    case 'GitHub':
+      return <Github color='#181717' size={18} />;
+    case 'Portfolio':
+      return <Logo width={17} />;
+    case 'Zenn':
+      return <Zenn color='#3EA8FF' size={18} />;
+  }
 }
