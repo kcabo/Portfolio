79276678806da5513fe31848b6f90da444907535
kcabo <37576764+kcabo@users.noreply.github.com>
2021-08-17 18:29:31 +0900

    ✨ make work tag dynamic

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,7 +1,7 @@
-export default function Tag() {
+export default function Tag(props: { text: string }) {
   return (
     <div className='px-4 py-1 text-sm font-medium text-gray-600 bg-gray-100 rounded-full'>
-      #LINE
+      {'#' + props.text}
     </div>
   );
 }
