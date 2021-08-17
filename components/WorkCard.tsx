1a92b2d5dbcd54decea0ee01996b8734aca349bd
kcabo <37576764+kcabo@users.noreply.github.com>
2021-08-17 20:06:37 +0900

    ✨ make work card dynamic

--- components/WorkCard.tsx
+++ components/WorkCard.tsx
@@ -3,14 +3,16 @@ import Link from 'next/link';
 import { ExternalLinkIcon } from '@heroicons/react/solid';
 import Tag from './Tag';
 
-export default function WorkCard() {
+import { WorkResponse } from '@/lib/types';
+
+export default function WorkCard({ data }: { data: WorkResponse }) {
   return (
-    <Link href='/works/[id]' as='/works/abc'>
+    <Link href={`/works/${encodeURIComponent(data.id)}`}>
       <a>
         <div className='overflow-hidden bg-white rounded-lg shadow-lg'>
           <div className='relative w-full h-48 border-b border-gray-300'>
             <Image
-              src='/tim.png'
+              src={data.coverImage.url}
               alt='サムネイル'
               layout='fill'
               objectFit='cover'
@@ -18,21 +20,22 @@ export default function WorkCard() {
           </div>
           <div className='px-5 py-6'>
             <div className='flex mb-2 align-baseline'>
-              <h4 className='mr-2 text-xl font-medium text-gray-600'>TiM</h4>
-              <Link href='/'>
-                <a className='px-1 pt-1'>
-                  <ExternalLinkIcon className='w-5 h-5 text-gray-400' />
-                </a>
-              </Link>
+              <h4 className='mr-2 text-xl font-medium text-gray-600'>
+                {data.title}
+              </h4>
+              {data.links?.homepage && (
+                <Link href={data.links.homepage}>
+                  <a className='px-1 pt-1'>
+                    <ExternalLinkIcon className='w-5 h-5 text-gray-400' />
+                  </a>
+                </Link>
+              )}
             </div>
-            <div className='mb-6 text-gray-500'>
-              部活でタイムを管理するためのLINE
-              Bot。対話形式でタイムを入力し、CSVに出力。
-            </div>
+            <div className='mb-6 text-gray-500'>{data.description}</div>
             <div className='flex flex-wrap gap-3'>
-              <Tag />
-              <Tag />
-              <Tag />
+              {data.tags.map((tag, index) => (
+                <Tag key={index} text={tag} />
+              ))}
             </div>
           </div>
         </div>
--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -1,7 +1,10 @@
 import WorkCard from '@/components/WorkCard';
 import PageTitle from '@/components/PageTitle';
 
-export default function Works() {
+import fetchWorks from '@/lib/fetchWorks';
+import { WorkResponse } from '@/lib/types';
+
+export default function Works({ works }: { works: WorkResponse[] }) {
   return (
     <div className='px-8'>
       <PageTitle
@@ -9,14 +12,18 @@ export default function Works() {
         sub='これまでに開発したアプリ・ツールの一覧です。'
       />
       <div className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3'>
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
+        {works.map((work, index) => (
+          <WorkCard key={index} data={work} />
+        ))}
       </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const works = await fetchWorks();
+
+  return {
+    props: { works },
+  };
 }
