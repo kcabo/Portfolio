875614555b6bbd05dcd676e0f0e6608ae925c33a
kcabo <37576764+kcabo@users.noreply.github.com>
2021-08-17 20:56:48 +0900

    ✨ fetch selected Works from API

--- components/Home/SelectedWorks.tsx
+++ components/Home/SelectedWorks.tsx
@@ -1,15 +1,17 @@
 import HomeCaption from './HomeCaption';
 import WorkCard from '../WorkCard';
 
-export default function SelectedWorks() {
+import { WorkResponse } from '@/lib/types';
+
+export default function SelectedWorks({ works }: { works: WorkResponse[] }) {
   return (
-    <div className=''>
+    <div>
       <HomeCaption caption='Selected Works' allLink='/works' />
       {/* トラックの高さ一杯にカードを広げる。見出しの高さを抜く */}
       <div className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3'>
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
+        {works.map((work, index) => (
+          <WorkCard key={index} data={work} />
+        ))}
       </div>
     </div>
   );
