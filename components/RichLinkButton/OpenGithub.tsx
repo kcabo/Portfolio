79e5243c314c71ba7c381219a1815763ce00ea1b
kcabo <k7cabo@gmail.com>
2021-08-23 19:02:10 +0900

    ✨ fetch repository last push date

--- components/RichLinkButton/OpenGithub.tsx
+++ components/RichLinkButton/OpenGithub.tsx
@@ -1,13 +1,16 @@
 import RichLinkButton from './RichLinkButton';
 import { Github } from '@icons-pack/react-simple-icons';
+import { useRepoDate } from '@/lib/githubAPI';
 
 export function OpenGithub({ url }: { url: string | undefined }) {
   if (!url) return <></>;
+  const repoUpdateDate = useRepoDate(url);
+
   return (
     <RichLinkButton
       icon={<Github size={40} />}
       mainText='View code on Github'
-      subText='last commit: 2020-08-14'
+      subText={`Last push: ${repoUpdateDate}`}
       buttonType='github'
       url={url}
     />
