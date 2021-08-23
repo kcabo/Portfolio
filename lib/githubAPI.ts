1d477654324f0600b4e80947f4e7f419883467c9
kcabo <37576764+kcabo@users.noreply.github.com>
2021-08-23 15:55:48 +0900

    ✨ set up commit-count API for badge

--- lib/githubAPI.ts
+++ lib/githubAPI.ts
@@ -5,14 +5,48 @@ const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN || '';
 const octokit = new Octokit({ auth: GITHUB_ACCESS_TOKEN });
 
 export async function fetchLatestPushDate() {
-  const response = await octokit.request('GET /users/{username}/events', {
-    username: 'kcabo',
-  });
+  try {
+    const response = await octokit.request('GET /users/{username}/events', {
+      username: 'kcabo',
+    });
 
-  // Eventの配列のうち、一番最初のPushEventが最新のもの
-  const latestPush = response.data.find((event) => event.type === 'PushEvent');
+    // Eventの配列のうち、一番最初のPushEventが最新のもの
+    const latestPush = response.data.find(
+      (event) => event.type === 'PushEvent'
+    );
+    if (!latestPush) throw new Error('Push Event not found');
 
-  const dateString = latestPush?.created_at;
+    const dateString = latestPush?.created_at;
+    if (!dateString) throw new Error('argument "created_at" not found');
 
-  return dateString ? toDate(dateString) : '-';
+    return toDate(dateString);
+  } catch (e) {
+    console.error(e);
+    return '-';
+  }
+}
+
+export async function fetchCommitCount(year: number) {
+  try {
+    const response: any = await octokit.graphql(
+      `query ($startFrom: DateTime!) {
+        viewer {
+          contributionsCollection(from: $startFrom) {
+            totalCommitContributions
+          }
+        }
+      }`,
+      { startFrom: `${year}-01-01T00:00:00Z` }
+    );
+
+    const totalCommit =
+      response?.viewer.contributionsCollection.totalCommitContributions;
+
+    if (!totalCommit) throw new Error('Response has no payload');
+
+    return String(totalCommit);
+  } catch (e) {
+    console.error(e);
+    return '-';
+  }
 }
--- pages/api/commit-count.ts
+++ pages/api/commit-count.ts
@@ -1,43 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-
-import { Octokit } from '@octokit/core';
-
-const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN || '';
-const octokit = new Octokit({ auth: GITHUB_ACCESS_TOKEN });
-
-async function fetchLatestPushDate() {
-  const response = await octokit.request('GET /users/{username}/events', {
-    username: 'kcabo',
-  });
-
-  const latestPush = response.data.find((event) => event.type === 'PushEvent');
-
-  const dateString = latestPush?.created_at;
-
-  return dateString ? toJSTDate(dateString) : '-';
-}
-
-function toJSTDate(dateString: string) {
-  const jst = new Date(dateString).toLocaleString('ja-JP');
-  return jst.split(' ')[0];
-}
-
-type Response = {
-  schemaVersion: 1;
-  label: string;
-  message: string;
-  color: string;
-};
+import { fetchCommitCount } from '@/lib/githubAPI';
+import { BadgeJsonSchema } from '@/lib/types';
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Response>
+  res: NextApiResponse<BadgeJsonSchema>
 ) {
-  const date = await fetchLatestPushDate();
-  res.status(200).json({
+  const year = 2021;
+  const commitCount = await fetchCommitCount(year);
+  const jsonSchema: BadgeJsonSchema = {
     schemaVersion: 1,
-    label: 'Last commit',
-    message: date,
+    label: `Commits in ${year}`,
+    message: commitCount,
     color: '#0496FF',
-  });
+  };
+  res.status(200).json(jsonSchema);
 }
