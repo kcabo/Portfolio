cc919fa37be16376708169da9b41b9a577b70502
kcabo <37576764+kcabo@users.noreply.github.com>
2021-08-23 16:12:08 +0900

    🐛 fixed layout shift for badges

--- components/Home/Badges.tsx
+++ components/Home/Badges.tsx
@@ -9,11 +9,13 @@ export default function Badges() {
       <img
         alt='Commit Count'
         src={`https://img.shields.io/endpoint?url=https%3A%2F%2F${BADGE_ENDPOINT}%2Fapi%2Fcommit-count`}
+        width={136}
         height={20}
       />
       <img
         alt='Last commit'
         src={`https://img.shields.io/endpoint?url=https%3A%2F%2F${BADGE_ENDPOINT}%2Fapi%2Flast-commit`}
+        width={152}
         height={20}
       />
     </div>
