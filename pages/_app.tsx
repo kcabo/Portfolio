d5f3d8756d1eebce77780aa523c2eeda4156811b
kcabo <37576764+kcabo@users.noreply.github.com>
2021-08-23 23:54:25 +0900

    ✨ Add static assets for browser

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,8 +10,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name='description' content='神崎伶央のポートフォリオサイト' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/logo.svg' />
-        {/* <link rel='apple-touch-icon' href='/apple-touch-icon.png' /> */}
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/icon.svg' type='image/svg+xml' />
+        <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+        <link rel='manifest' href='/site.webmanifest' />
+        <meta name='theme-color' content='#ffffff' />
         {/* <meta property='og:title' content='ページのタイトル' />
         <meta property='og:description' content='ページの説明文' />
         <meta property='og:image' content='https://mywebsite.com/image.jpg' />
