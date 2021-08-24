da00dd4c9a97de38b860f7a2c452108bc819013d
kcabo <k7cabo@gmail.com>
2021-08-24 17:37:28 +0900

    ♻️ optimize favicon svg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,22 +8,14 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Head>
-        <meta name='description' content='神崎伶央のポートフォリオサイト' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
         <link rel='icon' href='/icon.svg' type='image/svg+xml' />
+        <link rel='icon alternate' href='/favicon.ico' type='image/png' />
         <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
         <link rel='manifest' href='/site.webmanifest' />
         <meta name='theme-color' content='#ffffff' />
-        {/* <meta property='og:title' content='ページのタイトル' />
-        <meta property='og:description' content='ページの説明文' />
-        <meta property='og:image' content='https://mywebsite.com/image.jpg' />
-        <meta property='og:image:alt' content='画像の説明文' />
-        <meta property='og:locale' content='ja_JP' />
-        <meta property='og:type' content='website' />
+        <meta property='og:site_name' content='Reo Kanzaki | Portfolio' />
         <meta name='twitter:card' content='summary_large_image' />
-        <meta property='og:url' content='https://mywebsite.com/page' />
-        <link rel='canonical' href='https://mywebsite.com/page' /> */}
       </Head>
       <Component {...pageProps} />
     </Layout>
