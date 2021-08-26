9f26255a298bdebd737d755b8587cb387d8e67c8
kcabo <37576764+kcabo@users.noreply.github.com>
2021-08-26 23:13:31 +0900

    🐛 fix Log date

--- lib/fetchLogs.ts
+++ lib/fetchLogs.ts
@@ -1,5 +1,6 @@
 import { client } from './client';
 import { Log, LogListResponse } from './types';
+import { toDate } from './dateUtil';
 
 export default async function fetchLogs() {
   const response = await client.get<LogListResponse>({
@@ -18,7 +19,7 @@ function formatData(response: LogListResponse): Log[] {
   const logs = payload.map((item) => ({
     showOnTop: item.showOnTop,
     category: item.category[0], // 要素が一つでも配列形式が返されるため
-    date: item.date.slice(0, 10), // yyyy-MM-ddの部分を切り出す
+    date: toDate(item.date), // yyyy-MM-ddの部分を切り出す
     body: item.body,
   }));
 
