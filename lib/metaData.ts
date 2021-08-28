5c499bb679a9321f0985031c0b7d7d9351b694e1
kcabo <k7cabo@gmail.com>
2021-08-28 12:30:52 +0900

    🐛 enrich each page title

--- lib/metaData.ts
+++ lib/metaData.ts
@@ -1,13 +1,13 @@
 export const baseUrl = 'https://kcabo.vercel.app';
-export const homeTitle = 'Reo Kanzaki | Portfolio';
+export const homeTitle = 'Reo Kanzaki - Portfolio';
 export const homeDescription =
   '神崎伶央の個人サイトです。これまでに開発したソフトウェアや執筆した記事を紹介しています。';
-export const worksTitle = 'Works';
+export const worksTitle = 'Works | Reo Kanzaki';
 export const worksDescription =
   'これまでに開発したアプリ・ツールの一覧です。時系列順にしてあります。';
-export const aboutTitle = 'About';
+export const aboutTitle = 'About | Reo Kanzaki';
 export const aboutDescription = '私の使用可能言語や経歴について';
-export const privacyTitle = 'プライバシーポリシー';
+export const privacyTitle = 'プライバシーポリシー | Reo Kanzaki';
 export const privacyDescription = '当サイトのプライバシーポリシーについて';
-export const logsTitle = 'Logs';
+export const logsTitle = 'Logs | Reo Kanzaki';
 export const logsDescription = 'これまでの主な活動の記録';
