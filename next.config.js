4acf163bfb6960a487f2c8aa17f004129e9fce68
kcabo <37576764+kcabo@users.noreply.github.com>
2021-08-30 14:44:53 +0900

    ✨ PWA optimized

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: ['images.microcms-assets.io'],
   },
-};
+  pwa: {
+    dest: 'public',
+    dynamicStartUrl: false,
+  },
+});
