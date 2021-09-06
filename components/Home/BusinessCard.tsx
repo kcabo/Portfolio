64a2d330d8335c3e643953ec2d8e95e654317bad
kcabo <37576764+kcabo@users.noreply.github.com>
2021-09-06 21:19:13 +0900

    💄 apply animations, fix #2

--- components/Home/BusinessCard.tsx
+++ components/Home/BusinessCard.tsx
@@ -1,9 +1,20 @@
 import Image from 'next/image';
+import { useState } from 'react';
 import Badges from './Badges';
 
 export default function BusinessCard() {
+  const [animation, setAnimation] = useState(false);
+  function handleCardClick() {
+    setAnimation((prevState) => !prevState);
+  }
   return (
-    <div className='flex rounded-lg shadow-lg bg-[#292938] before:block before:pt-golden-main overflow-hidden'>
+    <div
+      className={
+        'flex overflow-hidden rounded-lg shadow-lg bg-[#292938] before:block before:pt-golden-main ' +
+        (animation ? 'animate-jello-horizontal' : '')
+      }
+      onClick={handleCardClick}
+    >
       <div className='flex flex-col justify-center pl-6 md:pl-9 w-golden-main'>
         <div className='mb-4 text-white'>
           <h1 className='text-xl font-bold lg:text-2xl'>Reo Kanzaki</h1>
