3369f3b4e1bd6db51ad4c2b525ff744396d081d3
kcabo <37576764+kcabo@users.noreply.github.com>
2022-01-05 09:46:49 +0000

    ♻️ simplify business card

--- components/Home/BusinessCard.tsx
+++ components/Home/BusinessCard.tsx
@@ -1,21 +1,10 @@
 import Image from 'next/image';
-import { useState } from 'react';
 import Badges from './Badges';
 
 export default function BusinessCard() {
-  const [animation, setAnimation] = useState(false);
-  function handleCardClick() {
-    setAnimation((prevState) => !prevState);
-  }
   return (
-    <div
-      className={
-        'flex overflow-hidden rounded-lg shadow-lg bg-[#292938] before:block before:pt-golden-main ' +
-        (animation ? 'animate-jello-horizontal' : '')
-      }
-      onClick={handleCardClick}
-    >
-      <div className='flex flex-col justify-center pl-6 md:pl-9 w-golden-main'>
+    <div className='flex overflow-hidden rounded-lg shadow-lg bg-[#292938] before:block before:pt-[61.8%]'>
+      <div className='flex flex-col justify-center pl-6 md:pl-9 w-[61.8%]'>
         <div className='mb-4 text-white'>
           <h1 className='text-xl font-bold lg:text-2xl'>Reo Kanzaki</h1>
           <p className='-mt-1 font-light opacity-50 lg:text-xl'>
@@ -24,13 +13,8 @@ export default function BusinessCard() {
         </div>
         <Badges />
       </div>
-      <div className='relative w-golden-rest'>
-        <Image
-          src='/me.webp'
-          alt='あーなたーとわーたしーが'
-          layout='fill'
-          objectFit='cover'
-        />
+      <div className='relative w-[38.3%]'>
+        <Image src='/me.webp' alt='神崎伶央' layout='fill' objectFit='cover' />
       </div>
     </div>
   );
