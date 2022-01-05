8dab176f8a788e7796ec6c5dc86dd41a0dadfef7
kcabo <37576764+kcabo@users.noreply.github.com>
2022-01-05 10:39:59 +0000

    💄 remove shields.io badge height

--- components/Home/Badges.tsx
+++ components/Home/Badges.tsx
@@ -5,17 +5,15 @@ const BADGE_ENDPOINT =
 
 export default function Badges() {
   return (
-    <div className='space-y-2'>
+    <div className='space-y-2 '>
       <img
         alt='Commit Count'
         src={`https://img.shields.io/endpoint?url=https%3A%2F%2F${BADGE_ENDPOINT}%2Fapi%2Fcommit-count`}
-        width={136}
         height={20}
       />
       <img
         alt='Last commit'
         src={`https://img.shields.io/endpoint?url=https%3A%2F%2F${BADGE_ENDPOINT}%2Fapi%2Flast-commit`}
-        width={152}
         height={20}
       />
     </div>
