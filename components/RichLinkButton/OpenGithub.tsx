2fa2df56b372dab5e2710df030481898001ec5da
kcabo <37576764+kcabo@users.noreply.github.com>
2025-02-26 23:14:40 +0900

    ♻️  refactor OpenGithub component and move useRepoDate to a separate function

--- components/RichLinkButton/OpenGithub.tsx
+++ components/RichLinkButton/OpenGithub.tsx
@@ -1,6 +1,10 @@
-import RichLinkButton from './RichLinkButton';
+'use client';
+
+import useSWR from 'swr';
 import { SiGithub } from '@icons-pack/react-simple-icons';
-import { useRepoDate } from '@/lib/githubAPI';
+
+import RichLinkButton from './RichLinkButton';
+import { toDate } from '@/lib/dateUtil';
 
 export function OpenGithub({ url }: { url: string }) {
   const repoUpdateDate = useRepoDate(url);
@@ -14,4 +18,55 @@ export function OpenGithub({ url }: { url: string }) {
       url={url}
     />
   );
+}
+
+export function useRepoDate(repoURL: string) {
+  const url = toAPIURL(repoURL);
+
+  const { data, error } = useSWR(url, safeFetcher, {
+    revalidateOnFocus: false, // ウィンドウがフォーカスされたときに自動的に再検証しない
+    dedupingInterval: 600000, // 10分以内のリクエストを重複として排除
+    errorRetryCount: 2,
+  });
+
+  if (error) {
+    return 'error!';
+  } else if (!error && !data) {
+    return 'Loading...';
+  } else if (data?.pushed_at) {
+    return toDate(data.pushed_at);
+  } else {
+    return '-';
+  }
+}
+
+async function safeFetcher(url: string) {
+  if (!url) throw new Error('URL not set');
+  const res = await window.fetch(url);
+  const json = await res.json();
+  return json;
+}
+
+function toAPIURL(repoURL: string) {
+  const { owner, repo } = extractRepoName(repoURL);
+  if (!owner) return null;
+  return `https://api.github.com/repos/${owner}/${repo}`;
+}
+
+function extractRepoName(repoURL: string) {
+  const re = /https?:\/\/github.com\/([A-Za-z0-9_.-]*)\/([A-Za-z0-9_.-]*)/;
+  const match = repoURL.match(re);
+
+  // owner と repo情報をURLから検出できない可能性もある
+  if (!match || match.length != 3) {
+    return {
+      owner: null,
+      repo: null,
+    };
+  } else {
+    return {
+      owner: match[1],
+      repo: match[2],
+    };
+  }
 }
--- lib/githubAPI.ts
+++ lib/githubAPI.ts
@@ -1,8 +1,5 @@
-'use client';
-
 import { Octokit } from '@octokit/core';
 import { toDate } from '@/lib/dateUtil';
-import useSWR from 'swr';
 
 const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN || '';
 const octokit = new Octokit({ auth: GITHUB_ACCESS_TOKEN });
@@ -48,56 +45,5 @@ export async function fetchCommitCount(year: number) {
   } catch (e) {
     console.error(e);
     return '-';
-  }
-}
-
-export function useRepoDate(repoURL: string) {
-  const url = toAPIURL(repoURL);
-
-  const { data, error } = useSWR(url, safeFetcher, {
-    revalidateOnFocus: false, // ウィンドウがフォーカスされたときに自動的に再検証しない
-    dedupingInterval: 600000, // 10分以内のリクエストを重複として排除
-    errorRetryCount: 2,
-  });
-
-  if (error) {
-    return 'error!';
-  } else if (!error && !data) {
-    return 'Loading...';
-  } else if (data?.pushed_at) {
-    return toDate(data.pushed_at);
-  } else {
-    return '-';
-  }
-}
-
-async function safeFetcher(url: string) {
-  if (!url) throw new Error('URL not set');
-  const res = await window.fetch(url);
-  const json = await res.json();
-  return json;
-}
-
-function toAPIURL(repoURL: string) {
-  const { owner, repo } = extractRepoName(repoURL);
-  if (!owner) return null;
-  return `https://api.github.com/repos/${owner}/${repo}`;
-}
-
-function extractRepoName(repoURL: string) {
-  const re = /https?:\/\/github.com\/([A-Za-z0-9_.-]*)\/([A-Za-z0-9_.-]*)/;
-  const match = repoURL.match(re);
-
-  // owner と repo情報をURLから検出できない可能性もある
-  if (!match || match.length != 3) {
-    return {
-      owner: null,
-      repo: null,
-    };
-  } else {
-    return {
-      owner: match[1],
-      repo: match[2],
-    };
   }
 }
